dd6766f8f41ae587e46c2a8bfa29e6305eaa2446
Marcin Ziolkowski <zziolko@clemson.edu>
2015-06-10 14:42:59 -0400

    NAMD2 example (Molecular dynamics

---
+++ 08-NAMD/job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#PBS -N NAMD2-test
+#PBS -l select=2:ncpus=2:mpiprocs=1:interconnect=fdr:mem=2gb
+#PBS -l walltime=1:00:00
+#PBS -j oe
+
+module load namd/20150610
+
+cd $PBS_O_WORKDIR
+mpirun namd2 ++ppn 2 alanin > alanin.output
